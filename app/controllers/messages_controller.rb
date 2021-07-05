62e8f37a66872a7308ce47023531f2c383287171
takurofukuta <mojuraspax@icloud.com>
2021-07-06 07:24:13 +0900

    CRUDを非同期処理で実装

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -2,12 +2,21 @@ class MessagesController < ApplicationController
   def index
     @messages = Message.all
   end
+  def new
+  end
   def create
     @message = Message.create!(message_params)
   end
   def destroy
     @message = Message.find(params[:id])
     @message.destroy!
+  end
+  def edit
+    @message = Message.find(params[:id])
+  end
+  def update
+    @message = Message.find(params[:id])
+    @message.update!(message_params)
   end
 
   private
